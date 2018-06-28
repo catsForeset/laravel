d946e4ccb0acf23a663bd5ccfb803197d4d4955c
luoyan <luoyaninneu@163.com>
2018-06-28 14:33:57 +0800

    首次提交

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,7 +1,14 @@
+{{--继承adminmuban.admin视图--}}
 @extends('adminmuban.admin')
-
+{{--标题部分--}}
+@section('title','index')
+{{--主部分--}}
 @section('main')
 <h1>后台admin</h1>
 <a type="button" href="/admin/create">添加</a>
 <a type="button" href="/admin/edit">修改</a>
-@endsection('main')
+@endsection
+
+@component('adminmuban.alert')
+    <strong>Whoops!</strong> Something went wrong!
+@endcomponent
